0f0294a511d7eb2c28a26afd2948f020671878cc
Rehan-Abhi <reinhbah987@gmail.com>
2026-01-02 17:04:17 +0000

    01_basics

---
+++ 01_basics/01_const,let,var.js
@@ -0,0 +1,11 @@
+const accountID = 123456
+let accountName = "Rehan don"
+var accountPassword = "234543"
+accountCity="Bathinda"
+// never use var
+/* variables can be declared without using keywords var and let, but it is not prefered
+*/
+accountName="The REHAN DON"
+accountPassword="1234321"
+accountCity="ludhiana"
+console.table([accountName, accountPassword, accountCity])
