9b355b684deb55cda1a0a71b33af74d2f7bf69bd
Rehan-Abhi <reinhbah987@gmail.com>
2026-01-03 06:36:17 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version of java script, waise nhi bhi lokhoge to bhi by defualt ye mmana jayega, but its a ood thing to mention
+
+// alert("hello") or alert(3+3) -> on browser it would have worked but as we are on node.js , it will give an error
+
+console.log(3+3) // we can aslo do arithmetic calculations in console.log
+let name ="rehan" // string datatype
+let age =20 //integer datatype
+let loggedIn = true // boolean datatype
+// normal integer range is till 2^53
+//bigint -> for even bigger numbers
+// null=> standlalone value representing empty value
+//undefined =>
+// symbol=> unique, 
+// object
+console.log(typeof "rehan") // answer -> string
+console.log(typeof null) // answer-> object IMPORTANT!!!!
+console.log(typeof undefined) // answer-> undefined
+
