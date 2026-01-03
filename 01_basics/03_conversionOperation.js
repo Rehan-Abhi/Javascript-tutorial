dcccab990869d329f65e517343b44f82ab0a69cf
Rehan-Abhi <reinhbah987@gmail.com>
2026-01-03 07:03:25 +0000

    conversions of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "33"
+console.log(typeof (score)) // -> string
+ let valueInNumber = Number(score) // -> now it has converted the string into a number
+ /* "33" -> 33
+ "33abc" -> NaN( not a number), NOTE-> THE DATATYPE OF NAN IS number
+ null-> 0 (dekho isne null ki value 0 de di, but ye iski galat baat ha
+ undefined -> NaN
+ true -> 1
+ false-> 0 */
+ let isLoogedIn =1
+ let booleanIsLoggedIn = Boolean(isLoggedIn) //-> now it has converted from number to boolean
+ /* 1-> true
+  0-> false
+  ""(emptystring) -> false
+  "rehan"(non empty string) -> true
+  */
+ let someNumber = 33
+ let stringSomeNumber = string(someNumber) // -> now it has converted from number to string
+ 
+
