e0cb70ed552103800ee2bd20f21bc718dca6b2ed
Rehan-Abhi <reinhbah987@gmail.com>
2026-07-15 08:09:10 +0000

    omission

--- 10_classes_and_oop/01_oop.js
+++ 10_classes_and_oop/01_oop.js
@@ -33,7 +33,10 @@ function User(username, loginCount, isLoggedIn){
 
 const userOne = new User("hitesh", 12, true) // this new keyword creates a new instance for the same method
 const userTwo = new User("ChaiAurCode", 11, false)
-console.log(userOne.constructor); // this will giv the reference of itself 
+console.log(userOne.constructor); //this will giv the reference of itself, this is because the User function has a prototype which contains a property called construction which refers to itsef that is the User function.
+
+console.log(User.prototype) //we expect to see constructor function here in the prototype but the output is {}, it is because the constrcutor property's enummerability is set ot false, that is the property is hidden
+
 //console.log(userTwo);
 
 // note-> if we do not use new keyword and keep on creating instances, then every instance will override its previous instance, because there is only one instance in that case.
